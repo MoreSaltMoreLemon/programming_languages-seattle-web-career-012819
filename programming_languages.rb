bf9264526922df8c7607bacc4547d480fc6144d5
github username <github email address>
2019-01-14 13:16:11 -0800

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,49 @@
-def reformat_languages(languages)
-  # your code here
+def reformat_languages(formatted_languages)
+  reformatted = {}
+  formatted_languages.each do |style, languages|
+    languages.each do |language, properties|
+      unless reformatted[language].nil?
+        styles = reformatted[language][:style]
+      else
+        styles = []
+      end
+      reformatted[language] = {
+        type: properties[:type],
+        style: styles << style
+      }
+    end
+  end
+  reformatted
+
 end
+
+languages = {
+  :oo => {
+    :ruby => {
+      :type => "interpreted"
+    },
+    :javascript => {
+      :type => "interpreted"
+    },
+    :python => {
+      :type => "interpreted"
+    },
+    :java => {
+      :type => "compiled"
+    }
+  },
+  :functional => {
+    :clojure => {
+      :type => "compiled"
+    },
+    :erlang => {
+      :type => "compiled"
+    },
+    :scala => {
+      :type => "compiled"
+    },
+    :javascript => {
+      :type => "interpreted"
+    }
+  }
+}
